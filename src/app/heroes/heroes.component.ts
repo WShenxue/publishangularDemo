3a9d9449a816158898c25e148360c580eb869263
WShenxue <wanshenxue@163.com>
2018-08-06 00:23:33 +0800

    Observable

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from './../hero';
 import { HEROES } from './../mock-heroes';
+import { HeroService } from './../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -14,13 +15,15 @@ export class HeroesComponent implements OnInit {
     name: "Widstorm"
   }
 
-  heroes = HEROES;
+  //heroes = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   //public onSelect(hero) {
@@ -29,5 +32,10 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    //this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 }
