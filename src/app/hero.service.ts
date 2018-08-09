557fe0b626c9b75026c3f30659017aacc890fa27
WShenxue <wanshenxue@163.com>
2018-08-09 23:45:46 +0800

    hero operator

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -6,6 +6,11 @@ import { catchError, map, tap } from 'rxjs/operators';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './messgae.service';
+import { HttpHandler } from '@angular/common/http/src/backend';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'content-type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
@@ -24,13 +29,19 @@ export class HeroService {
     this.messageService.add('HeroService: fetched heroes');
     //return of(HEROES);
     return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      tap(heroes => this.log('fetched heroes')), 
       catchError(this.handleError('getHeroes', []))
     );
   }
 
   getHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
     this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    //return of(HEROES.find(hero => hero.id === id));
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    )
   }
 
   private log(message: string) {
@@ -43,6 +54,31 @@ export class HeroService {
       this.log(`${operation} faild: ${error.message}`);
       return of(result as T);
     }
+  }
+
+
+  updateHero(hero: Hero): Observable<any> {
+    
+    return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
+      tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === "number" ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete<Hero>(url, httpOptions).pipe(
+      tap(_ => this.log(`delete hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
   }
 
 }
